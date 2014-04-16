9426d72c321fe617a3fe39945d1a07cc26739a25
Dr Potato <ismay@ismaywolff.nl>
2014-04-16 11:56:08 +0200

    Added example to nav-dropdown.js

--- js/src/nav-dropdown.js
+++ js/src/nav-dropdown.js
@@ -1,3 +1,10 @@
+// *************************************
+//
+//   Navigation dropdown
+//   -> Example: http://codepen.io/anon/pen/FymDB
+//
+// *************************************
+
 $(document).ready(function() {
     $('.has-dropdown').click(function() {
         $('.dropdown').toggleClass('is-hidden');
