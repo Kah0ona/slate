c3b97b2096ef5110be8aa1c7022822080c8db3b4
dr-potato <ismay@lokaalgevonden.nl>
2014-04-28 13:29:09 +0200

    Forked child and parent templates, addition of sitemap include

---
+++ src/php/includes/sitemap.php
@@ -0,0 +1,7 @@
+<?php wp_nav_menu(array(
+    'container'=> 'nav',
+    'menu' => 'sitemap',
+    'container_class' =>'Select Select--sitemap',
+    'theme_location' => 'sitemap',
+    'items_wrap' => '<ul class="%2$s">%3$s</ul>'
+)); ?>
---
+++ src/php/templates/functions.child
@@ -0,0 +1,10 @@
+<?php
+  // Register the main navigation
+  add_action( 'init', 'register_menu_sitemap' );
+
+  function register_menu_sitemap(){
+  register_nav_menus( array(
+  'sitemap' => 'Sitemap'
+      ));
+  }
+?>
