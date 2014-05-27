6c5f19f5afdfe74e8f506c3c021416cd1df2e854
Marten Sytema <marten@sytematic.nl>
2014-05-27 10:35:49 +0200

    Update svgstore.js
    added u-hide class to SVG

--- util/grunt/svgstore.js
+++ util/grunt/svgstore.js
@@ -10,7 +10,8 @@ module.exports = {
     options: {
       prefix : 'icon-',
       svg: {
-        viewBox : '0 0 100 100'
+        viewBox : '0 0 100 100',
+        "class" : "u-hide"
       }
     },
     files: {
